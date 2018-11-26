b0078d43104deb55d3581a8986d2b268ad6d582f
Brandon Callifornia <BCallifornia@users.noreply.github.com>
2018-11-26 23:42:58 +0100

    U mac.sh A docker, docker-compose, docker-machine completions

--- mac.sh
+++ mac.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+ln -s docker-completion.bash /usr/local/etc/bash_completion.d/docker
+ln -s docker-compose-completion.bash /usr/local/etc/bash_completion.d/docker-compose
+ln -s docker-machine-completion.bash /usr/local/etc/bash_completion.d/docker-machine
 ln -s git-full-completion.bash /usr/local/etc/bash_completion.d/git
 ln -s kubectl-completion.bash /usr/local/etc/bash_completion.d/kubectl
 ln -s minikube-completion.bash /usr/local/etc/bash_completion.d/minikube
