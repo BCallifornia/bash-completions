55a0593491dc4d18ccd45c6e9b5898578841343c
Brandon Callifornia <BCallifornia@users.noreply.github.com>
2018-11-26 20:27:04 +0100

    A/C mac.sh links all completions into /usr/local/etc/bash_completion.d/

---
+++ mac.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+ln -s git-full-completion.bash /usr/local/etc/bash_completion.d/git
+ln -s kubectl-completion.bash /usr/local/etc/bash_completion.d/kubectl
+ln -s minikube-completion.bash /usr/local/etc/bash_completion.d/minikube
