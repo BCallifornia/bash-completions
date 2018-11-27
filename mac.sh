dab60e499d8aa91218ebc5b42ee7f188ce54fb70
Brandon Callifornia <BCallifornia@users.noreply.github.com>
2018-11-27 23:20:43 +0100

    U mac.sh A/C packer-completion.bash

--- mac.sh
+++ mac.sh
@@ -5,3 +5,4 @@ ln -s docker-machine-completion.bash /usr/local/etc/bash_completion.d/docker-mac
 ln -s git-full-completion.bash /usr/local/etc/bash_completion.d/git
 ln -s kubectl-completion.bash /usr/local/etc/bash_completion.d/kubectl
 ln -s minikube-completion.bash /usr/local/etc/bash_completion.d/minikube
+ln -s packer-completion.bash /usr/local/ets/bash_completion.d/packer
