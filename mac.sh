245dac02ac22c9eaa441c797be6d582355eeb3ef
Brandon Callifornia <BCallifornia@users.noreply.github.com>
2018-11-30 12:40:45 +0100

    U mac.sh A basepath to correct linking

--- mac.sh
+++ mac.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
-ln -s docker-completion.bash /usr/local/etc/bash_completion.d/docker
-ln -s docker-compose-completion.bash /usr/local/etc/bash_completion.d/docker-compose
-ln -s docker-machine-completion.bash /usr/local/etc/bash_completion.d/docker-machine
-ln -s git-full-completion.bash /usr/local/etc/bash_completion.d/git
-ln -s kubectl-completion.bash /usr/local/etc/bash_completion.d/kubectl
-ln -s minikube-completion.bash /usr/local/etc/bash_completion.d/minikube
-ln -s packer-completion.bash /usr/local/etc/bash_completion.d/packer
+PWD=$(pwd)
+echo "Basepath to link from: $PWD"
+ln -s "$PWD/docker-completion.bash" /usr/local/etc/bash_completion.d/docker
+ln -s "$PWD/docker-compose-completion.bash" /usr/local/etc/bash_completion.d/docker-compose
+ln -s "$PWD/docker-machine-completion.bash" /usr/local/etc/bash_completion.d/docker-machine
+ln -s "$PWD/git-full-completion.bash" /usr/local/etc/bash_completion.d/git
+ln -s "$PWD/kubectl-completion.bash" /usr/local/etc/bash_completion.d/kubectl
+ln -s "$PWD/minikube-completion.bash" /usr/local/etc/bash_completion.d/minikube
+ln -s "$PWD/packer-completion.bash" /usr/local/etc/bash_completion.d/packer
